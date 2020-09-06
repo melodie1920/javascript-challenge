70ed653cee597ab61a31aedf4b94145bb4f0c39e
melodie1920 <63599612+melodie1920@users.noreply.github.com>
2020-09-06 10:06:06 -0400

    Adding initial logic for filtering on date

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,11 +4,36 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 tableData.forEach(function(ufoData) {
-    console.log(ufoData);
     var row = tbody.append("tr");
     Object.entries(ufoData).forEach(function([key, value]) {
-        console.log(key, value);
         var cell = row.append("td");
         cell.text(value);
     });
 });
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", dateselect);
+form.on("submit", dateselect);
+
+function dateselect() {
+
+    var inputElement = d3.select("#datetime");
+
+    var inputValue = inputElement.property("value");
+
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+    filteredData.forEach(function(ufoData) {
+        var row = tbody.append("tr");
+        Object.entries(ufoData).forEach(function([key, value]) {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+}
