a3838c84d69960df9041c03afa301a1b9d48ae38
melodie1920 <63599612+melodie1920@users.noreply.github.com>
2020-09-06 12:08:37 -0400

    Added logic for multiple filters level 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -17,6 +17,8 @@ var button = d3.select("#filter-btn");
 // Select the form
 var form = d3.select("#form");
 
+var filters = [];
+
 // Create event handlers 
 button.on("click", dateselect);
 form.on("submit", dateselect);
@@ -25,11 +27,61 @@ function dateselect() {
 
     d3.event.preventDefault();
 
-    var inputElement = d3.select("#datetime");
+    var inputElementDate = d3.select("#datetime");
+    var inputElementCity = d3.select("#city");
+    var inputElementState = d3.select("#state");
+    var inputElementCountry = d3.select("#country");
+    var inputElementShape = d3.select("#shape");
 
-    var inputValue = inputElement.property("value");
+    var inputValueDate = inputElementDate.property("value");
+    var inputValueCity = inputElementCity.property("value");
+    var inputValueState = inputElementState.property("value");
+    var inputValueCountry = inputElementCountry.property("value");
+    var inputValueShape = inputElementShape.property("value");
 
-    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+    if (inputValueDate.length > 0) {
+        filters.push({"datetime":inputValueDate});
+    }
+
+    if (inputValueCity.length > 0) {
+        filters.push({"city":inputValueCity});
+    }
+
+    if (inputValueState.length > 0) {
+        filters.push({"state":inputValueState});
+    }
+
+    if (inputValueCountry.length > 0) {
+        filters.push({"country":inputValueCountry});
+    }
+
+    if (inputValueShape.length > 0) {
+        filters.push({"shape":inputValueShape});
+    }
+
+    filteredData = tableData;
+
+    filters.forEach((filter) => {
+        Object.entries(filter).forEach(([key, value]) => {
+            if (key === "datetime") {
+                filteredData = filteredData.filter(filteredData => filteredData.datetime === value);
+            }
+            if (key === "city") {
+                filteredData = filteredData.filter(filteredData => filteredData.city === value);
+            }
+            if (key === "state") {
+                filteredData = filteredData.filter(filteredData => filteredData.state === value);
+            }
+            if (key === "country") {
+                filteredData = filteredData.filter(filteredData => filteredData.country === value);
+            }
+            if (key === "shape") {
+                filteredData = filteredData.filter(filteredData => filteredData.shape === value);
+            }
+        });
+    });
+
+    //console.log(filteredData);
 
     document.getElementsByTagName('tbody')[0].innerHTML = '';
 
