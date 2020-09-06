22b9a6ebd70248af953f3391d3881fd2213d2bb2
melodie1920 <63599612+melodie1920@users.noreply.github.com>
2020-09-06 09:42:09 -0400

    Added table to html file

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,14 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach(function(ufoData) {
+    console.log(ufoData);
+    var row = tbody.append("tr");
+    Object.entries(ufoData).forEach(function([key, value]) {
+        console.log(key, value);
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
